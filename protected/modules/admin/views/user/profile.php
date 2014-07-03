fb1ba0b87c2edd7b4dedd9500c8ff0b78953e02a
rodwin <rodwinlising@gmail.com>
2014-07-03 16:22:48 +0800

    delete noc.bak

---
+++ protected/modules/admin/views/user/profile.php
@@ -0,0 +1,11 @@
+<?php
+$this->breadcrumbs=array(
+	'Users'=>array('admin'),
+	$model->user_name=>array('view','id'=>$model->user_id),
+	'Update',
+);
+
+	?>
+
+
+<?php echo $this->renderPartial('_form',array('model'=>$model,'listCompanies'=>$listCompanies)); ?>
