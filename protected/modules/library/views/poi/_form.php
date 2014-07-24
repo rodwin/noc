73d2ed623d6cbc445e0d412f67da4f7fe18a9e9c
jttorate <kalibo06@in1go.com.ph>
2014-07-24 09:01:44 +0800

    add barangay, municipal, province, region models then configure poi setup

--- protected/modules/library/views/poi/_form.php
+++ protected/modules/library/views/poi/_form.php
@@ -14,7 +14,7 @@
                 <?php /* if($form->errorSummary($model)){?><div class="alert alert-danger alert-dismissable">
                   <i class="fa fa-ban"></i>
                   <?php echo $form->errorSummary($model); ?></div>
-                 */ ?>   
+                 */ ?>
 
                 <input id="poi_id" type="hidden" value="<?php echo $model->poi_id; ?>"/>
 
@@ -22,81 +22,135 @@
                     <?php echo $form->textFieldGroup($model, 'short_name', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 200)))); ?>
                 </div>
 
-
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'long_name', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 250)))); ?>
                 </div>
-
 
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'primary_code', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 100)))); ?>
                 </div>
 
-
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'secondary_code', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 100)))); ?>
                 </div>
-
-
-                <div class="form-group">
-                    <?php echo $form->textFieldGroup($model, 'barangay_id', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5')))); ?>
-                </div>
-
-
-                <div class="form-group">
-                    <?php echo $form->textFieldGroup($model, 'municipal_id', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5')))); ?>
-                </div>
-
-
-                <div class="form-group">
-                    <?php echo $form->textFieldGroup($model, 'province_id', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5')))); ?>
-                </div>
-
-
-                <div class="form-group">
-                    <?php echo $form->textFieldGroup($model, 'region_id', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5')))); ?>
-                </div>
-
 
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'sales_region_id', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5')))); ?>
                 </div>
 
+                <div class="form-group">
+                    <?php // echo $form->textFieldGroup($model, 'region_id', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5')))); ?>
+                    <?php
+                    echo $form->dropDownListGroup(
+                            $model, 'region_id', array(
+                        'wrapperHtmlOptions' => array(
+                            'class' => 'col-sm-5',
+                        ),
+                        'widgetOptions' => array(
+                            'data' => $region,
+                            'htmlOptions' => array('multiple' => false, 'prompt' => 'Select Region',
+                                'ajax' => array(
+                                    'type' => 'POST',
+                                    'url' => CController::createUrl('poi/getProvinceByRegionCode'),
+                                    'update' => '#province',
+                                    'data' => array('region_code' => 'js:this.value',),
+                                )),
+                        )
+                            )
+                    );
+                    ?>
+                </div>
+
+                <div class="form-group">
+                    <?php // echo $form->textFieldGroup($model, 'province_id', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5')))); ?>
+                    <?php
+                    echo $form->dropDownListGroup(
+                            $model, 'province_id', array(
+                        'wrapperHtmlOptions' => array(
+                            'class' => 'col-sm-5',
+                        ),
+                        'widgetOptions' => array(
+                            'data' => $province,
+                            'htmlOptions' => array('multiple' => false, 'prompt' => 'Select Province', 'id' => 'province',
+                                'ajax' => array(
+                                    'type' => 'POST',
+                                    'url' => CController::createUrl('poi/getMunicipalByProvinceCode'),
+                                    'update' => '#municipal',
+                                    'data' => array('province_code' => 'js:this.value',),
+                                )),
+                        )
+                            )
+                    );
+                    ?>
+                </div>
+
+                <div class="form-group">
+                    <?php // echo $form->textFieldGroup($model, 'municipal_id', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5')))); ?>
+                    <?php
+                    echo $form->dropDownListGroup(
+                            $model, 'municipal_id', array(
+                        'wrapperHtmlOptions' => array(
+                            'class' => 'col-sm-5',
+                        ),
+                        'widgetOptions' => array(
+                            'data' => $municipal,
+                            'htmlOptions' => array('multiple' => false, 'prompt' => 'Select Municipal', 'id' => 'municipal',
+                                'ajax' => array(
+                                    'type' => 'POST',
+                                    'url' => CController::createUrl('poi/getBarangayByMunicipalCode'),
+                                    'update' => '#barangay',
+                                    'data' => array('municipal_code' => 'js:this.value',),
+                                )),
+                        )
+                            )
+                    );
+                    ?>
+                </div>
+
+                <div class="form-group">
+                    <?php // echo $form->textFieldGroup($model, 'barangay_id', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5')))); ?>
+                    <?php
+                    echo $form->dropDownListGroup(
+                            $model, 'barangay_id', array(
+                        'wrapperHtmlOptions' => array(
+                            'class' => 'col-sm-5',
+                        ),
+                        'widgetOptions' => array(
+                            'data' => $barangay,
+                            'htmlOptions' => array('multiple' => false, 'prompt' => 'Select Barangay', 'id' => 'barangay'),
+                        )
+                            )
+                    );
+                    ?>
+                </div>
 
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'latitude', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 9)))); ?>
                 </div>
 
-
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'longitude', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 9)))); ?>
                 </div>
-
 
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'address1', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 200)))); ?>
                 </div>
 
-
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'address2', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 200)))); ?>
                 </div>
-
 
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'zip', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 50)))); ?>
                 </div>
 
-
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'landline', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 50)))); ?>
                 </div>
 
-
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'mobile', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 50)))); ?>
                 </div>
-
 
                 <div class="form-group">
                     <?php //echo $form->textFieldGroup($model,'poi_category_id',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
@@ -138,36 +192,29 @@
                     ?>
                 </div>
 
-
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'remarks', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 250)))); ?>
                 </div>
-
 
                 <div class="form-group">
                     <?php echo $form->textFieldGroup($model, 'status', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5', 'maxlength' => 50)))); ?>
                 </div>
 
-
                 <div class="form-group">
                     <?php //echo $form->textFieldGroup($model,'edited_date',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5'))));  ?>
                 </div>
-
 
                 <div class="form-group">
                     <?php //echo $form->textFieldGroup($model,'edited_by',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>100))));  ?>
                 </div>
 
-
                 <div class="form-group">
                     <?php //echo $form->textFieldGroup($model,'verified_by',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>100))));  ?>
                 </div>
 
-
                 <div class="form-group">
                     <?php //echo $form->textFieldGroup($model,'verified_date',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5'))));  ?>
                 </div>
-
 
                 <div id="custom_datas">
 
