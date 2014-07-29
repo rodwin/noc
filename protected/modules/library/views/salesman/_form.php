5dfc581d02f4193f409d8caf755a0c8808347896
rodwin <rodwinlising@gmail.com>
2014-07-29 15:35:15 +0800

    Divided salesman's _form to 2 div

--- protected/modules/library/views/salesman/_form.php
+++ protected/modules/library/views/salesman/_form.php
@@ -3,7 +3,7 @@
 <div class="panel panel-default">
     
   <div class="panel-body">
-    <div class="col-md-8">
+    <div class="col-md-12">
 <?php $form=$this->beginWidget('booster.widgets.TbActiveForm',array(
 	'id'=>'salesman-form',
 	'enableAjaxValidation'=>false,
@@ -14,55 +14,70 @@
     <?php echo $form->errorSummary($model); ?></div>
 */ ?>        
 
-	
-	<div class="form-group">
+	<div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
 		<?php echo $form->textFieldGroup($model,'team_leader_id',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
-	</div>
+                </div>
+
+
+                <div class="form-group">
+                        <?php echo $form->textFieldGroup($model,'salesman_name',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>200)))); ?>
+                </div>
+
+
+                <div class="form-group">
+                        <?php echo $form->textFieldGroup($model,'salesman_code',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
+                </div>
+
+
+                <div class="form-group">
+                        <?php echo $form->textFieldGroup($model,'mobile_number',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
+                </div>
+
+
+                <div class="form-group">
+                        <?php echo $form->textFieldGroup($model,'device_no',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
+                </div>
+                
+                <div class="form-group">
+                        <?php echo $form->textFieldGroup($model,'is_team_leader',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>1)))); ?>
+                </div>
+
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                        <?php echo $form->textFieldGroup($model,'other_fields_1',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
+                </div>
+
+
+                <div class="form-group">
+                        <?php echo $form->textFieldGroup($model,'other_fields_2',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
+                </div>
+
+
+                <div class="form-group">
+                        <?php echo $form->textFieldGroup($model,'other_fields_3',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
+                </div>
+            </div>
+	
 
 	
-	<div class="form-group">
-		<?php echo $form->textFieldGroup($model,'salesman_name',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>200)))); ?>
-	</div>
+	
 
 	
-	<div class="form-group">
-		<?php echo $form->textFieldGroup($model,'salesman_code',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
-	</div>
-
 	
-	<div class="form-group">
-		<?php echo $form->textFieldGroup($model,'mobile_number',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
-	</div>
-
-	
-	<div class="form-group">
-		<?php echo $form->textFieldGroup($model,'device_no',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
-	</div>
-
-	
-	<div class="form-group">
-		<?php echo $form->textFieldGroup($model,'other_fields_1',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
-	</div>
-
-	
-	<div class="form-group">
-		<?php echo $form->textFieldGroup($model,'other_fields_2',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
-	</div>
-
-	
-	<div class="form-group">
-		<?php echo $form->textFieldGroup($model,'other_fields_3',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>50)))); ?>
-	</div>
-
-	
-	<div class="form-group">
-		<?php echo $form->textFieldGroup($model,'is_team_leader',array('widgetOptions'=>array('htmlOptions'=>array('class'=>'span5','maxlength'=>1)))); ?>
-	</div>
 
 
-<div class="form-group">
-    <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save',array('class'=>'btn btn-primary btn-flat')); ?>    <?php echo CHtml::resetButton('Reset',array('class'=>'btn btn-primary btn-flat')); ?></div>
-
+        
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group">
+                    <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save',array('class'=>'btn btn-primary btn-flat')); ?>    <?php echo CHtml::resetButton('Reset',array('class'=>'btn btn-primary btn-flat')); ?>
+                </div>
+            </div>
+        </div>
 <?php $this->endWidget(); ?>
     </div>
   </div>
