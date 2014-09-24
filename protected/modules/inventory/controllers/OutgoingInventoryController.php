3a2af29f6003074171b474419516032aae5a746c
jttorate <kalibo06@in1go.com.ph>
2014-09-24 08:20:11 +0800

    -- outgoing controller updates and update incoming form upload button click

--- protected/modules/inventory/controllers/OutgoingInventoryController.php
+++ protected/modules/inventory/controllers/OutgoingInventoryController.php
@@ -30,7 +30,7 @@ class OutgoingInventoryController extends Controller {
                 'users' => array('@'),
             ),
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                'actions' => array('create', 'update', 'data', 'loadInventoryDetails', 'outgoingInvDetailData', 'afterDeleteTransactionRow', 'invData', 'uploadAttachment' , 'preview', 'deleteByUrl', 'download'),
+                'actions' => array('create', 'update', 'data', 'loadInventoryDetails', 'outgoingInvDetailData', 'afterDeleteTransactionRow', 'invData', 'uploadAttachment', 'preview', 'deleteByUrl', 'download'),
                 'users' => array('@'),
             ),
             array('allow', // allow admin user to perform 'admin' and 'delete' actions
@@ -366,7 +366,7 @@ class OutgoingInventoryController extends Controller {
                 default:
                     break;
             }
-            
+
             $row['outgoing_inventory_detail_id'] = $value->outgoing_inventory_detail_id;
             $row['outgoing_inventory_id'] = $value->outgoing_inventory_id;
             $row['batch_no'] = $value->batch_no;
@@ -442,8 +442,8 @@ class OutgoingInventoryController extends Controller {
     public function actionDelete($id) {
         if (Yii::app()->request->isPostRequest) {
             // we only allow deletion via POST request
-           $this->deleteByUrl($id); 
-           $this->loadModel($id)->delete();
+            $this->deleteByUrl($id);
+            $this->loadModel($id)->delete();
 
             // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
             if (!isset($_GET['ajax'])) {
@@ -509,150 +509,149 @@ class OutgoingInventoryController extends Controller {
             Yii::app()->end();
         }
     }
-    
-     public function actionUploadAttachment() {
-      header('Vary: Accept');
-      if (isset($_SERVER['HTTP_ACCEPT']) &&
-              (strpos($_SERVER['HTTP_ACCEPT'], 'application/json') !== false)) {
-         header('Content-type: application/json');
-      } else {
-         header('Content-type: text/plain');
-      }
 
-      $data = array();
-      $model = new Attachment;
+    public function actionUploadAttachment() {
+        header('Vary: Accept');
+        if (isset($_SERVER['HTTP_ACCEPT']) &&
+                (strpos($_SERVER['HTTP_ACCEPT'], 'application/json') !== false)) {
+            header('Content-type: application/json');
+        } else {
+            header('Content-type: text/plain');
+        }
 
-      if (isset($_FILES['Attachment']['name']) && $_FILES['Attachment']['name'] != "") {
+        $data = array();
+        $model = new Attachment;
 
-         $file = CUploadedFile::getInstance($model, 'file');
+        if (isset($_FILES['Attachment']['name']) && $_FILES['Attachment']['name'] != "") {
+
+            $file = CUploadedFile::getInstance($model, 'file');
 //         $dir = dirname(Yii::app()->getBasePath()) . DIRECTORY_SEPARATOR . 'attachment' . DIRECTORY_SEPARATOR . Yii::app()->user->company_id . DIRECTORY_SEPARATOR .Yii::app()->session['tid'];
-         $dir = dirname(Yii::app()->getBasePath()) . DIRECTORY_SEPARATOR . 'protected' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . Yii::app()->user->company_id . DIRECTORY_SEPARATOR . 'attachments' . DIRECTORY_SEPARATOR . 'outgoing' . DIRECTORY_SEPARATOR . Yii::app()->session['tid'];
-         if (!is_dir($dir)) {
-            mkdir($dir, 0777, true);
-         }
+            $dir = dirname(Yii::app()->getBasePath()) . DIRECTORY_SEPARATOR . 'protected' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . Yii::app()->user->company_id . DIRECTORY_SEPARATOR . 'attachments' . DIRECTORY_SEPARATOR . 'outgoing' . DIRECTORY_SEPARATOR . Yii::app()->session['tid'];
+            if (!is_dir($dir)) {
+                mkdir($dir, 0777, true);
+            }
 
-         $file_name = str_replace(' ', '_', strtolower($file->name));
+            $file_name = str_replace(' ', '_', strtolower($file->name));
 //         $url = Yii::app()->getBaseUrl(true) . '/attachment/' . Yii::app()->user->company_id . '/' . Yii::app()->session['tid'] . '/' . $file_name;
-         $url = Yii::app()->getBaseUrl(true) . '/protected/uploads/' . Yii::app()->user->company_id . '/attachments/outgoing/' . Yii::app()->session['tid'] . '/' . $file_name;
-         $file->saveAs($dir . DIRECTORY_SEPARATOR . $file_name);
+            $url = Yii::app()->getBaseUrl(true) . '/protected/uploads/' . Yii::app()->user->company_id . '/attachments/outgoing/' . Yii::app()->session['tid'] . '/' . $file_name;
+            $file->saveAs($dir . DIRECTORY_SEPARATOR . $file_name);
 
-         $model->attachment_id = Globals::generateV4UUID();
+            $model->attachment_id = Globals::generateV4UUID();
 
-         $model->company_id = Yii::app()->user->company_id;
-         $model->file_name = $file_name;
-         $model->url = $url;
-         $model->transaction_id = Yii::app()->session['tid'];
-         $model->transaction_type = 'outgoing';
-         $model->created_by = Yii::app()->user->name;
+            $model->company_id = Yii::app()->user->company_id;
+            $model->file_name = $file_name;
+            $model->url = $url;
+            $model->transaction_id = Yii::app()->session['tid'];
+            $model->transaction_type = 'outgoing';
+            $model->created_by = Yii::app()->user->name;
 
-         if ($model->save()) {
+            if ($model->save()) {
 
-            $data[] = array(
-                'name' => $file->name,
-                'type' => $file->type,
-                'size' => $file->size,
-                'url' => $dir . DIRECTORY_SEPARATOR . $file_name,
-                'thumbnail_url' => $dir . DIRECTORY_SEPARATOR . $file_name,
+                $data[] = array(
+                    'name' => $file->name,
+                    'type' => $file->type,
+                    'size' => $file->size,
+                    'url' => $dir . DIRECTORY_SEPARATOR . $file_name,
+                    'thumbnail_url' => $dir . DIRECTORY_SEPARATOR . $file_name,
 //                    'delete_url' => $this->createUrl('my/delete', array('id' => 1, 'method' => 'uploader')),
 //                    'delete_type' => 'POST',
-            );
-         } else {
+                );
+            } else {
 
-            if ($model->hasErrors()) {
+                if ($model->hasErrors()) {
 
-               $data[] = array('error', $model->getErrors());
+                    $data[] = array('error', $model->getErrors());
+                }
             }
-         }
-      } else {
+        } else {
 
-         throw new CHttpException(500, "Could not upload file " . CHtml::errorSummary($model));
-      }
+            throw new CHttpException(500, "Could not upload file " . CHtml::errorSummary($model));
+        }
 
 
-      echo json_encode($data);
-   }
+        echo json_encode($data);
+    }
 
-   function actionPreview($id) {
-      $c = new CDbCriteria;
-      $c->compare("company_id", Yii::app()->user->company_id);
-      $c->compare("transaction_id", $id);
-      $attachment = Attachment::model()->findAll($c);
+    function actionPreview($id) {
+        $c = new CDbCriteria;
+        $c->compare("company_id", Yii::app()->user->company_id);
+        $c->compare("transaction_id", $id);
+        $attachment = Attachment::model()->findAll($c);
 
-      $output = array();
-      foreach ($attachment as $key => $value) {
-         $row = array();
-         $row['file_name'] = $value->file_name;
-         $row['links'] = '<a class="view" title="Download" data-toggle="tooltip" href="' . $this->createUrl('/inventory/outgoinginventory/download', array('id' => $value->attachment_id)) . '" data-original-title="Download"><button type="submit" class="btn btn-default btn-flat">
+        $output = array();
+        foreach ($attachment as $key => $value) {
+            $row = array();
+            $row['file_name'] = $value->file_name;
+            $row['links'] = '<a class="view" title="Download" data-toggle="tooltip" href="' . $this->createUrl('/inventory/outgoinginventory/download', array('id' => $value->attachment_id)) . '" data-original-title="Download"><button type="submit" class="btn btn-default btn-flat">
             <i class="icon-download icon-white"></i>
             <i class="glyphicon glyphicon-download"></i>
            
             </button></a>'
-                 . '&nbsp;<a class="delete" title="Delete" data-toggle="tooltip" href="' . $this->createUrl('/inventory/outgoinginventory/deletebyurl', array('id' => $value->attachment_id)) . '" data-original-title="Delete"><button type="button" class="btn btn-default btn-flat">
+                    . '&nbsp;<a class="delete" title="Delete" data-toggle="tooltip" href="' . $this->createUrl('/inventory/outgoinginventory/deletebyurl', array('id' => $value->attachment_id)) . '" data-original-title="Delete"><button type="button" class="btn btn-default btn-flat">
             <i class="glyphicon glyphicon-trash"></i>
             
             </button></a>';
 
-         $output['data'][] = $row;
-      }
+            $output['data'][] = $row;
+        }
 
-      echo json_encode($output);
-   }
+        echo json_encode($output);
+    }
 
-   
-   function actionDeleteByUrl($id) {
+    function actionDeleteByUrl($id) {
 
-      $sql = "SELECT url FROM noc.attachment WHERE attachment_id = :attachment_id AND company_id = '". Yii::app()->user->company_id ."'";
+        $sql = "SELECT url FROM noc.attachment WHERE attachment_id = :attachment_id AND company_id = '" . Yii::app()->user->company_id . "'";
 
-      $command = Yii::app()->db->createCommand($sql);
-      $command->bindParam(':attachment_id', $id, PDO::PARAM_STR);
-      $data = $command->queryAll();
-      foreach ($data as $key => $value) {
-         $url = $value['url'];
-      }
-      //$url = substr($url, 16);
-      $base = Yii::app()->getBaseUrl(true);
-      $arr = explode("/", $base);
-      $base = $arr[count($arr) - 1];
-      $url = str_replace(Yii::app()->getBaseUrl(true), "", $url);
-      //pre('../' .$base . $url);
-      unlink('../' . $base . $url);
+        $command = Yii::app()->db->createCommand($sql);
+        $command->bindParam(':attachment_id', $id, PDO::PARAM_STR);
+        $data = $command->queryAll();
+        foreach ($data as $key => $value) {
+            $url = $value['url'];
+        }
+        //$url = substr($url, 16);
+        $base = Yii::app()->getBaseUrl(true);
+        $arr = explode("/", $base);
+        $base = $arr[count($arr) - 1];
+        $url = str_replace(Yii::app()->getBaseUrl(true), "", $url);
+        //pre('../' .$base . $url);
+        unlink('../' . $base . $url);
 
-      $sql = "DELETE FROM noc.attachment WHERE attachment_id = :attachment_id AND company_id = '". Yii::app()->user->company_id ."'";
+        $sql = "DELETE FROM noc.attachment WHERE attachment_id = :attachment_id AND company_id = '" . Yii::app()->user->company_id . "'";
 
-      $command = Yii::app()->db->createCommand($sql);
-      $command->bindParam(':attachment_id', $id, PDO::PARAM_STR);
-      $data = $command->query();
+        $command = Yii::app()->db->createCommand($sql);
+        $command->bindParam(':attachment_id', $id, PDO::PARAM_STR);
+        $data = $command->query();
 
-      $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-   }
+        $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+    }
 
-   public function actionDownload($id) {
+    public function actionDownload($id) {
 
-      $sql = "SELECT url, file_name FROM noc.attachment WHERE attachment_id = :attachment_id AND company_id = '". Yii::app()->user->company_id ."'";
+        $sql = "SELECT url, file_name FROM noc.attachment WHERE attachment_id = :attachment_id AND company_id = '" . Yii::app()->user->company_id . "'";
 
-      $command = Yii::app()->db->createCommand($sql);
-      $command->bindParam(':attachment_id', $id, PDO::PARAM_STR);
-      $data = $command->queryAll();
-      foreach ($data as $key => $value) {
-         $url = $value['url'];
-         $name = $value['file_name'];
-      }
-      $model = new ReceivingInventory;
+        $command = Yii::app()->db->createCommand($sql);
+        $command->bindParam(':attachment_id', $id, PDO::PARAM_STR);
+        $data = $command->queryAll();
+        foreach ($data as $key => $value) {
+            $url = $value['url'];
+            $name = $value['file_name'];
+        }
+        $model = new ReceivingInventory;
 
 
 //      $name = $_GET['file'];
 //      $upload_path = Yii::app()->params['uploadPath'];
-       $base = Yii::app()->getBaseUrl(true);
-      $arr = explode("/", $base);
-      $base = $arr[count($arr) - 1];
-      $url = str_replace(Yii::app()->getBaseUrl(true), "", $url);
-     
-      if (file_exists('../' . $base . $url)) {
-         Yii::app()->getRequest()->sendFile($name, file_get_contents('../' . $base . $url));
-         $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-      } else {
-         
-      }
-   }
+        $base = Yii::app()->getBaseUrl(true);
+        $arr = explode("/", $base);
+        $base = $arr[count($arr) - 1];
+        $url = str_replace(Yii::app()->getBaseUrl(true), "", $url);
+
+        if (file_exists('../' . $base . $url)) {
+            Yii::app()->getRequest()->sendFile($name, file_get_contents('../' . $base . $url));
+            $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+        } else {
+            
+        }
+    }
 
 }
