cf10a8d1c5885883b05f8e92b3ca3ec018a7f5fa
jttorate <kalibo06@in1go.com.ph>
2014-09-24 08:23:57 +0800

    -- change inventory dashboard label to Inventory Management

--- protected/views/layouts/_menu.php
+++ protected/views/layouts/_menu.php
@@ -3,7 +3,7 @@ $main_menu = array(
     array('label' => 'Dashboard', 'url' => array('site/index'), 'icon' => 'fa fa-dashboard', 'visible' => !Yii::app()->user->isGuest),
     array('label' => 'Location Viewer', 'url' => array('locationviewer'), 'icon' => 'fa fa-map-marker', 'visible' => !Yii::app()->user->isGuest),
     array('label' => 'Inventory', 'url' => '#', 'icon' => 'fa fa-list-alt', 'visible' => !Yii::app()->user->isGuest, 'items' => array(
-            array('label' => 'Dashboard', 'icon' => 'fa fa-angle-double-right', 'url' => array('/inventory/inventory/admin'), 'visible' => !Yii::app()->user->isGuest),
+            array('label' => 'Inventory Management', 'icon' => 'fa fa-angle-double-right', 'url' => array('/inventory/inventory/admin'), 'visible' => !Yii::app()->user->isGuest),
             array('label' => 'Receiving', 'icon' => 'fa fa-angle-double-right', 'url' => array('/inventory/receivingInventory/admin'), 'visible' => !Yii::app()->user->isGuest),
             array('label' => 'Incoming', 'icon' => 'fa fa-angle-double-right', 'url' => array('/inventory/incomingInventory/admin'), 'visible' => !Yii::app()->user->isGuest),
             array('label' => 'Outgoing', 'icon' => 'fa fa-angle-double-right', 'url' => array('/inventory/outgoingInventory/admin'), 'visible' => !Yii::app()->user->isGuest),
