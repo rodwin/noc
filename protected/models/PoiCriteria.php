b8e577a522b65844933abcfaac20736026847eef
julius <in1go@yahoo.com>
2015-02-03 17:35:03 +0800

    Commit for Online Inventory Webservice

---
+++ protected/models/PoiCriteria.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PoiCriteria
+ *
+ * @author julius
+ */
+class PoiCriteria {
+
+   /**
+    * @var string company_id
+    * @soap
+    */
+   public $company_id;
+
+   /**
+    * @var string sku_id
+    * @soap
+    */
+   public $sku_id;
+
+   /**
+    * @var string uom_id
+    * @soap
+    */
+   public $uom_id;
+
+   /**
+    * @var string unit_price
+    * @soap
+    */
+   public $unit_price;
+   
+   /**
+    * @var string quantity_received
+    * @soap
+    */
+   public $quantity_received;
+   
+   /**
+    * @var string zone_id
+    * @soap
+    */
+   public $zone_id;
+   
+   /**
+    * @var string transaction_date
+    * @soap
+    */
+   public $transaction_date;
+  
+   /**
+    * @var string created_by
+    * @soap
+    */
+   public $created_by;
+   
+   /**
+    * @var string expiration_date
+    * @soap
+    */
+   public $expiration_date;
+   
+   /**
+    * @var string reference_no
+    * @soap
+    */
+   public $reference_no;
+   
+   
+   /**
+    * @var string status_id
+    * @soap
+    */
+   public $status_id;
+   
+   /**
+    * @var string po_no
+    * @soap
+    */
+   public $po_no;
+   
+   /**
+    * @var string pr_no
+    * @soap
+    */
+   public $pr_no;
+   
+   /**
+    * @var string pr_date
+    * @soap
+    */
+   public $pr_date;
+   
+   /**
+    * @var string plan_arrival_date
+    * @soap
+    */
+   public $plan_arrival_date;
+   
+
+}
